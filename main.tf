973cb6fa65cf6409521d079449b03db8c7a3114b
Ellis McDougald <ecmcdougald@gmail.com>
2024-07-18 15:58:54 -0600

    Provision GetTeamRatings lambda function

--- main.tf
+++ main.tf
@@ -61,11 +61,34 @@ resource "aws_iam_role_policy" "dynamodb-lambda-policy" {
    })
 }
 
-resource "aws_lambda_function" "terraform_basic_lambda_func" {
+resource "aws_lambda_function" "dayhandler_lambda_func" {
 filename                       = "DayHandler/src/DayHandler/lambda.zip"
 function_name                  = "DayHandler_Function"
 role                           = aws_iam_role.iam_for_lambda.arn
 handler                        = "DayHandler::DayHandler.Function::FunctionHandler"
 runtime                        = "dotnet8"
 timeout			       = 60
+}
+
+resource "aws_lambda_function" "getteamratings_lambda_func" {
+filename                       = "GetTeamRatings/src/GetTeamRatings/lambda.zip"
+function_name                  = "GetTeamRatings_Function"
+role                           = aws_iam_role.iam_for_lambda.arn
+handler                        = "GetTeamRatings::GetTeamRatings.Function::FunctionHandler"
+runtime                        = "dotnet8"
+timeout			       = 60
+}
+
+resource "aws_lambda_function_url" "getteamratings_lambda_url" {
+  function_name      = aws_lambda_function.getteamratings_lambda_func.function_name
+  authorization_type = "NONE"
+
+  cors {
+    allow_credentials = true
+    allow_origins     = ["*"]
+    allow_methods     = ["*"]
+    allow_headers     = ["date", "keep-alive"]
+    expose_headers    = ["keep-alive", "date"]
+    max_age           = 86400
+  }
 }
