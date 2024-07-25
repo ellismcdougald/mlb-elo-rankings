27e835f9ac86151f5bf91c7ba2170f415fbe11c8
Ellis McDougald <ecmcdougald@gmail.com>
2024-07-25 16:10:14 -0600

    Make table page the default

--- website/src/app/app.routes.ts
+++ website/src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
-import { TableComponent } from './table/table.component'
-import { HowItWorksComponent } from './how-it-works/how-it-works.component'
+import { TableComponent } from './table/table.component';
+import { HowItWorksComponent } from './how-it-works/how-it-works.component';
 
 export const routes: Routes = [
-  { path: 'elo-table', component: TableComponent },
+  { path: '', component: TableComponent },
   { path: 'how-it-works', component: HowItWorksComponent },
 ];
